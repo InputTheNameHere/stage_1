873888e15a700072cb330427e57f5a4d7ad14bc9
leono <leonoorbarton@outlook.com>
2025-10-02 00:31:14 +0100

    implement date-hour datalake layout

--- scripts/download_book.py
+++ scripts/download_book.py
@@ -1,10 +1,22 @@
 import requests
 from pathlib import Path
+from datetime import datetime
+
 START_MARKER="*** START OF THE PROJECT GUTENBERG EBOOK"
 END_MARKER="*** END OF THE PROJECT GUTENBERG EBOOK"
-def download_book(book_id:int,output_path:str):
-    output_path=Path(output_path)
+
+def download_book(book_id:int):
+
+    base_path = Path(__file__).resolve().parents[1]
+    datalake_path = base_path / "data storage" / "datalake"
+
+    now = datetime.now()
+    date_str = now.strftime("%Y%m%d")
+    hour_str = now.strftime("%H")
+    output_path= datalake_path / date_str / hour_str
+
     output_path.mkdir(parents=True,exist_ok=True)
+
     url=f"https://www.gutenberg.org/cache/epub/{book_id}/pg{book_id}.txt"
     response=requests.get(url)
     response.raise_for_status()
@@ -12,6 +24,7 @@ def download_book(book_id:int,output_path:str):
 
     if START_MARKER not in text or END_MARKER not in text:
         return False
+
     header,body_and_footer=text.split(START_MARKER,1)
     body,footer=body_and_footer.split(END_MARKER,1)
 
@@ -24,4 +37,4 @@ def download_book(book_id:int,output_path:str):
         f.write(header.strip())
     return True
 
-success= download_book(1342,"data/output")
+download_book(1342)
